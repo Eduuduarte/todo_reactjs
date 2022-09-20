7f4936fa8dba627ac07322d70031313142329a50
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-09-19 21:27:36 -0300

    finish project

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,31 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { ListItem } from './components/ListItem'
+import {AddArea} from './components/AddArea';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
     {id: 1, name: 'comprar o pão na padaria', done: false},
-    {id: 2, name: 'comprar um bolo na padaria', done: false}
+    {id: 2, name: 'comprar um bolo na padaria', done: true}
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  const handleChangeStatus = (id: number, checked: boolean) => {
+    let newList = [...list];
+    const changeDone = newList.findIndex(item => item.id === id);
+    newList[changeDone].done = checked;
+    setList(newList);
+    console.log(changeDone, newList);
+  }
   
   return (
     <C.Container>
@@ -15,13 +34,14 @@ const App = () => {
         <C.Header>Lista de Tarefas</C.Header>
 
         {/* Área de adicionar nova tarefa */}
-
-        {list.map((item, index) => (
-          <ListItem key={index} item={item} />
-        ))}
-
+        
+        <AddArea onEnter={handleAddTask}/>
 
         {/* Lista de tarefas */}
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} changeDone={handleChangeStatus}/>
+        ))}
       </C.Area>
     </C.Container>
   );
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,14 +1,28 @@
 import * as C from './styles';
 import { Item } from '../../types/Item';
+import { useState } from 'react';
 
 type Props = {
     item: Item;
+    changeDone: (id: number, checked: boolean) => void;
 }
 
-export const ListItem = ({item}: Props) => {
-    return(
-        <C.Container>
-            {item.name}
+export const ListItem = ({ item, changeDone }: Props) => {
+    const [isChecked, setIsChecked] = useState(item.done);
+
+    const handleChange = () => {
+        changeDone(item.id, !isChecked);
+        setIsChecked(!isChecked);
+    }
+
+    return (
+        <C.Container done={item.done}>
+            <input
+                type="checkbox"
+                checked={item.done}
+                onChange={handleChange}
+            />
+            <label>{item.name}</label> 
         </C.Container>
     );
 }
